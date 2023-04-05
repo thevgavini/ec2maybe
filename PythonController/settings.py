f66f5ba45993b8146de0ac78ac1e380acb7244aa
thevgavini <77669880+thevgavini@users.noreply.github.com>
2023-04-05 05:39:45 +0530

    Update settings.py

--- PythonController/settings.py
+++ PythonController/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-# STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_buid", "static")
+# STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build", "static")
